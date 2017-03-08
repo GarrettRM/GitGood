a8f9fcaa30962a1812938a44588f644b4f05367c
han5ol <han.s.lee@csra.com>
2017-03-08 13:09:57 -0600

    Angular 1.1
    -Edit function
    -Delete function
    -few change in main page

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { AppServices } from './service/app.service';
 export class AppComponent{
     posts : Post[];
     show: boolean;
+    show2: boolean;
     name: string;
     constructor(private appService: AppServices) {
         this.name = "Admin";
@@ -21,10 +22,25 @@ export class AppComponent{
     onClick(){
       if(this.show==false){
         this.show = true;
+        this.show2= false;
       }
       else{
         this.show = false;
       }
+    }
+
+    onClick2(){
+      if(this.show2==false){
+        this.show2 = true;
+        this.show= false;
+      }
+      else{
+        this.show2 = false;
+      }
+    }
+
+    delRow(i: number){
+      this.posts.splice(i, 1)
     }
 
 
